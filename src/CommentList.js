87de4e557756c16f4d57631e62f8dbc13b49d354
Mohammad Khafid Barokum <khafid@majoo.id>
2021-02-07 19:45:42 +0700

    complete quick start

--- src/CommentList.js
+++ src/CommentList.js
@@ -4,7 +4,19 @@ import React from 'react';
 
 export default ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content;
+        break;
+      case 'rejected':
+        content = 'This comment has been rejected';
+        break;
+      default:
+        content = 'This comment is awaiting moderation';
+        break;
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
